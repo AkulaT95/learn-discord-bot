a77d00f58fb7278b3477803c6e68df16058627ec
Tuan Dang Ho Anh <dangtuansapper@gmail.com>
2024-01-10 21:34:35 +0700

    Init app

---
+++ index.js
@@ -0,0 +1,9 @@
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+const { token } = require('./config.json');
+
+const client = new Client({intents:[GatewayIntentBits.Guilds]});
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(token);
